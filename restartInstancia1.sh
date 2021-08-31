c5095c84e2f6dbe0921c241200d2a0f5cdb8c05f
AndresCrg <brayan.cardenas@uptc.edu.co>
2021-08-31 16:56:47 -0500

    Se modifica el script de restaurar instancia

--- restartInstancia1.sh
+++ restartInstancia1.sh
@@ -1,5 +1,5 @@
 docker rm instancia1
 docker rmi instancia1img
-docker builder prune
+docker builder prune --force
 docker build -t instancia1img .
 docker run -d --name instancia1 -p 9801:9800 --net mynet --ip 119.18.0.11 instancia1img
--- restartInstancia2.sh
+++ restartInstancia2.sh
@@ -1,5 +1,5 @@
 docker rm instancia2
 docker rmi instancia2img
-docker builder prune
-docker build -t instancia1img .
-docker run -d --name instancia1 -p 9801:9800 --net mynet --ip 119.18.0.11 instancia2img
+docker builder prune --force
+docker build -t instancia2img .
+docker run -d --name instancia2 -p 9801:9800 --net mynet --ip 119.18.0.11 instancia2img
