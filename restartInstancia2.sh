4685ee99681ade1d5f438c00e26772cb9d005414
Dario Leonidas Baron <47702340+drox13@users.noreply.github.com>
2021-08-31 02:04:51 -0500

    Update restartInstancia2.sh

--- restartInstancia2.sh
+++ restartInstancia2.sh
@@ -2,4 +2,4 @@ docker rm instancia2
 docker rmi instancia2img
 docker builder prune
 docker build -t instancia1img .
-docker run -d --name instancia1 -p 9801:9800 --net mynet --ip 119.18.0.11 instancias2img
+docker run -d --name instancia1 -p 9801:9800 --net mynet --ip 119.18.0.11 instancia2img
