78edfce5752a9b13c21799cce39fd4f6adef36ff
Dario Leonidas Baron <47702340+drox13@users.noreply.github.com>
2021-08-31 02:02:56 -0500

    se borra la imagen de cache

--- restartInstancia1.sh
+++ restartInstancia1.sh
@@ -1,4 +1,5 @@
 docker rm instancia1
 docker rmi instancia1img
+docker builder prune
 docker build -t instancia1img .
 docker run -d --name instancia1 -p 9801:9800 --net mynet --ip 119.18.0.11 instancias1img
