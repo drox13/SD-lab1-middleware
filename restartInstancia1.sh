86eb835dcdefe82d4644b73876af606d7b7bc712
drox13 <dario.baron@uptc.edu.co>
2021-08-31 17:41:56 -0500

    Entregable

--- restartInstancia1.sh
+++ restartInstancia1.sh
@@ -1,9 +1,6 @@
 docker rm instancia1
 docker rmi instancia1img
 docker builder prune --force
-<<<<<<< HEAD
-docker build -t instancia1img .
-=======
+
 docker build --no-cache -t instancia1img .
->>>>>>> 9d0a09da5fbebab702352e0848b17d76f1896dbb
 docker run -d --name instancia1 -p 9801:9800 --net mynet --ip 119.18.0.11 instancia1img
--- restartInstancia2.sh
+++ restartInstancia2.sh
@@ -1,9 +1,10 @@
 docker rm instancia2
 docker rmi instancia2img
 docker builder prune --force
-<<<<<<< HEAD
-docker build -t instancia2img .
-=======
+
 docker build --no-cache -t instancia2img .
->>>>>>> 9d0a09da5fbebab702352e0848b17d76f1896dbb
-docker run -d --name instancia2 -p 9801:9800 --net mynet --ip 119.18.0.11 instancia2img
+docker run -d --name instancia2 -p 9802:9800 --net mynet --ip 119.18.0.22 instancia2img
+
+#docker start instancia2
+#docker exec instancia1 bash -c "git pull; pm2 stop inde.js; pm2 start inde.js"
+
