6d1b1140f0c3b3660936f9810082784fee861d5f
drox13 <dario.baron@uptc.edu.co>
2021-08-31 17:58:22 -0500

    PMV

--- restartInstancia2.sh
+++ restartInstancia2.sh
@@ -6,5 +6,4 @@ docker build --no-cache -t instancia2img .
 docker run -d --name instancia2 -p 9802:9800 --net mynet --ip 119.18.0.22 instancia2img
 
 #docker start instancia2
-#docker exec instancia1 bash -c "git pull; pm2 stop inde.js; pm2 start inde.js"
-
+#docker exec instancia1 bash -c "git pull; pm2 stop index.js; pm2 start index.js"
