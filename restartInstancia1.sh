4b7436f6587614f2c4f02325acce621fa009711f
Dario Leonidas Baron <47702340+drox13@users.noreply.github.com>
2021-08-31 01:05:32 -0500

    se borra la instancia1 y se restaura

--- restartInstancia1.sh
+++ restartInstancia1.sh
@@ -1,1 +1,4 @@
-docker start instancia1
+docker rm instancia1
+docker rmi instancia1img
+docker build -t instancia1img .
+docker run -d --name instancia1 -p 9801:9800 --net mynet --ip 119.18.0.11 instancias1img
