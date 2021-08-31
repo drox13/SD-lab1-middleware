c83c6ff5fb30ed64bc10d6e2f91ed91a46b6db51
drox13 <dario.baron@uptc.edu.co>
2021-08-31 18:32:18 -0500

    Entregable Funcional

--- restartInstancia1.sh
+++ restartInstancia1.sh
@@ -1,6 +1,9 @@
-docker rm instancia1
-docker rmi instancia1img
-docker builder prune --force
+# docker rm instancia1
+# docker rmi instancia1img
+# docker builder prune --force
 
-docker build --no-cache -t instancia1img .
-docker run -d --name instancia1 -p 9801:9800 --net mynet --ip 119.18.0.11 instancia1img
+# docker build --no-cache -t instancia1img .
+# docker run -d --name instancia1 -p 9801:9800 --net mynet --ip 119.18.0.11 instancia1img
+
+docker start instancia1
+docker exec instancia1 bash -c "git pull; pm2 stop index.js; pm2 start index.js"
--- restartInstancia2.sh
+++ restartInstancia2.sh
@@ -1,9 +1,9 @@
-docker rm instancia2
-docker rmi instancia2img
-docker builder prune --force
+# docker rm instancia2
+# docker rmi instancia2img
+# docker builder prune --force
 
-docker build --no-cache -t instancia2img .
-docker run -d --name instancia2 -p 9802:9800 --net mynet --ip 119.18.0.22 instancia2img
+# docker build --no-cache -t instancia2img .
+# docker run -d --name instancia2 -p 9802:9800 --net mynet --ip 119.18.0.22 instancia2img
 
-#docker start instancia2
-#docker exec instancia1 bash -c "git pull; pm2 stop index.js; pm2 start index.js"
+docker start instancia2
+docker exec instancia2 bash -c "git pull; pm2 stop index.js; pm2 start index.js"
